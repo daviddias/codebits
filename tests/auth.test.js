d978641a51fed527e1bde63874f7b36f4c020f1c
David Dias <daviddias.p@gmail.com>
2014-03-16 11:35:06 -0700

    added auth test

---
+++ tests/auth.test.js
@@ -0,0 +1,15 @@
+process.env.NODE_ENV = 'test';
+var tap      = require('tap');
+var test     = tap.test;
+var codebits = require('./../index.js');
+var secret   = require('./secret.json');  
+var token    = require('./../modules/token.js');
+
+test('Auth test', function(t) {
+  t.equal(token.getToken(), null, 'Token should be empty before');
+  codebits.auth.logIn(secret.user, secret.password, function (err, _token){
+    t.type(_token,'string', 'Token should be a string');
+    t.equal(_token, token.getToken(), 'Stored Token should be equal');
+    t.end();
+  });
+});

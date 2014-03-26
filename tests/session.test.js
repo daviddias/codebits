c08195bec534380b391d287f2f5e7b55fdde91bb
afatc <alexandrefilipecampos@gmail.com>
2014-03-26 01:29:04 +0000

    Session info implemented and tested

---
+++ tests/session.test.js
@@ -0,0 +1,21 @@
+process.env.NODE_ENV = 'test';
+var tap = require('tap');
+var test = tap.test;
+var codebits = require('./../index.js');
+var secret = require('./secret.json');
+var token = require('./../modules/token.js');
+
+
+test('Search by name', function(t){
+  
+  var id = '110';
+  codebits.session.sessionInfo(id, function(err, result){
+    
+    t.type(result, 'string', 'Result should be a string');
+    var res = JSON.parse(result);
+    t.type(res.id, 'string', 'Should be a number');
+    t.equal(res.id, id, 'Should have the same id');
+    t.end();
+
+  });
+});
